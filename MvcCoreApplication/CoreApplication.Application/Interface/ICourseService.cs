081cda4b57c63d2d79cc3f32810faad7be6aff6b
Kamal.Kumar <Kamal.Kumar@MLBBTL-12543>
2020-06-29 18:04:22 +0530

    Added new MVC Project in core 3.1 and added application project

---
+++ MvcCoreApplication/CoreApplication.Application/Interface/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using CoreApplication.Application.ViewModels;
+using System.Collections.Generic;
+
+namespace CoreApplication.Application.Interface
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses(); 
+    }
+}
---
+++ MvcCoreApplication/CoreApplication.Domain/Interface/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoreApplication.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApplication.Domain.Interface
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
