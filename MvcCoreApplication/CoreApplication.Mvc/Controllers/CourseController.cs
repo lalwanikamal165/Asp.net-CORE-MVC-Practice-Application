b2984e123f80f8fd7d9bcca2f80de7495fb5ab91
Kamal.Kumar <Kamal.Kumar@MLBBTL-12543>
2020-06-30 14:39:35 +0530

    Added new Page course and created intial migration for new db context

---
+++ MvcCoreApplication/CoreApplication.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApplication.Application.Interface;
+using CoreApplication.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApplication.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
